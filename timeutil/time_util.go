e05ec50d6ef05f0197cedf309bab8d10ed506d64
Crayon <614820984@qq.com>
2022-09-24 16:22:03 +0800

    [UPDATE] update time tool

--- timeutil/time_util.go
+++ timeutil/time_util.go
@@ -9,28 +9,51 @@ type Time struct {
 	time.Time
 }
 
-// UnixMilli 毫秒时间戳
-func UnixMilli(t time.Time) int64 {
-	return t.UnixNano() / int64(time.Millisecond)
-}
-
+// Now 获取现在时间
 func Now() *Time {
 	return &Time{
-		Time: time.Now(),
+		time.Now(),
 	}
 }
 
+// UnixSec 根据秒级时间戳获取Time
+func UnixSec(sec int64) *Time {
+	return &Time{
+		time.Unix(sec, 0),
+	}
+}
+
+// UnixMilli 根据毫秒级时间戳获取Time
+func UnixMilli(milli int64) *Time {
+	return &Time{
+		time.Unix(milli/1000, 0),
+	}
+}
+
+// UnixMilli 获取毫秒时间戳
+func (t *Time) UnixMilli() int64 {
+	return t.UnixNano() / int64(time.Millisecond)
+}
+
+// UnixSec 获取秒级时间戳
+func (t *Time) UnixSec() int64 {
+	return t.UnixNano() / int64(time.Second)
+}
+
+// BeginOfSecond 获取秒的开始
 func (t *Time) BeginOfSecond() *Time {
 	t.Time = time.Unix(0, t.UnixNano()-int64(t.Nanosecond()))
 	return t
 }
 
+// BeginOfMinute 获取分钟的开始
 func (t *Time) BeginOfMinute() *Time {
 	t.BeginOfSecond()
 	t.Time = time.Unix(t.Unix()-int64(t.Second()), 0)
 	return t
 }
 
+// BeginOfHour 获取小时的开始
 func (t *Time) BeginOfHour() *Time {
 	t.BeginOfMinute()
 	m := t.Minute()
@@ -39,6 +62,7 @@ func (t *Time) BeginOfHour() *Time {
 	return t
 }
 
+// BeginOfDay 获取天的开始
 func (t *Time) BeginOfDay() *Time {
 	t.BeginOfHour()
 	h := t.Hour()
@@ -47,33 +71,40 @@ func (t *Time) BeginOfDay() *Time {
 	return t
 }
 
+// OffsetDays 偏移days天
 func (t *Time) OffsetDays(days int) *Time {
 	t.Time = t.AddDate(0, 0, days)
 	return t
 }
 
+// OffsetMonths 偏移months月
 func (t *Time) OffsetMonths(months int) *Time {
 	t.Time = t.AddDate(0, 1, 0)
 	return t
 }
 
+// OffsetYears 偏移years年
 func (t *Time) OffsetYears(years int) *Time {
 	t.Time = t.AddDate(1, 0, 0)
 	return t
 }
 
+// Tomorrow 明天
 func (t *Time) Tomorrow() *Time {
 	return t.OffsetDays(1)
 }
 
+// AfterTomorrow 后天
 func (t *Time) AfterTomorrow() *Time {
 	return t.OffsetDays(2)
 }
 
+// Yesterday 昨天
 func (t *Time) Yesterday() *Time {
 	return t.OffsetDays(-1)
 }
 
+// BeforeYesterday 前天
 func (t *Time) BeforeYesterday() *Time {
 	return t.OffsetDays(-2)
 }
@@ -87,7 +118,23 @@ func DiffDay(t1, t2 *Time) int64 {
 	return (ts2 - ts1) / int64(24*time.Hour)
 }
 
+/*
+1. 秒数只差小于一天（绝对相差不到一天），那么只需要判断是否同一天即可
+2. 对于绝对相差大于一天的情况，减掉绝对相差的秒数转到情况1即可
+*/
 // DiffHumanDay 相差自然日
 func DiffHumanDay(t1, t2 *Time) int64 {
-	return 0
+	ts1, ts2 := t1.UnixNano(), t2.UnixNano()
+	if ts1 > ts2 {
+		ts1, ts2 = ts2, ts1
+		t1, t2 = t2, t1
+	}
+	absDay := (ts2 - ts1) / int64(24*time.Hour)
+	tt := t2
+	if absDay >= 1 {
+		tt = &Time{
+			time.Unix(0, ts2-absDay*int64(24*time.Hour)),
+		}
+	}
+	return absDay + int64(tt.Day()-t1.Day())
 }
--- timeutil/time_util_test.go
+++ timeutil/time_util_test.go
@@ -5,20 +5,188 @@ import (
 	"time"
 )
 
-func TestUnixMilli(t *testing.T) {
+func TestDiffHumanDay(t *testing.T) {
 	type args struct {
-		t time.Time
+		t1 *Time
+		t2 *Time
 	}
 	tests := []struct {
 		name string
 		args args
+		want int64
 	}{
 		// TODO: Add test cases.
-		{name: "", args: args{t: time.Now()}},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-24 16:00:00
+					time.Unix(1664006400, 0),
+				},
+				t2: &Time{
+					// 2022-09-25 01:00:00
+					time.Unix(1664038800, 0),
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-24 23:00:00
+					time.Unix(1664031600, 0),
+				},
+				t2: &Time{
+					// 2022-09-25 01:00:00
+					time.Unix(1664038800, 0),
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-24 12:00:00
+					time.Unix(1663992000, 0),
+				},
+				t2: &Time{
+					// 2022-09-24 23:00:00
+					time.Unix(1664031600, 0),
+				},
+			},
+			want: 0,
+		},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-25 23:30:00
+					time.Unix(1664119800, 0),
+				},
+				t2: &Time{
+					// 2022-09-24 23:00:00
+					time.Unix(1664031600, 0),
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-26 01:00:00
+					time.Unix(1664125200, 0),
+				},
+				t2: &Time{
+					// 2022-09-24 23:00:00
+					time.Unix(1664031600, 0),
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "",
+			args: struct {
+				t1 *Time
+				t2 *Time
+			}{
+				t1: &Time{
+					// 2022-09-28 23:00:00
+					time.Unix(1664377200, 0),
+				},
+				t2: &Time{
+					// 2022-09-24 01:00:00
+					time.Unix(1664031600, 0),
+				},
+			},
+			want: 4,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			t.Logf("UnixMilli: %d\n", UnixMilli(tt.args.t))
+			if got := DiffHumanDay(tt.args.t1, tt.args.t2); got != tt.want {
+				t.Errorf("DiffHumanDay() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDiffDay(t *testing.T) {
+	type args struct {
+		t1 *Time
+		t2 *Time
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{
+			name: "",
+			args: args{
+				t1: &Time{
+					// 2022-09-26 01:00:00
+					time.Unix(1664125200, 0),
+				},
+				t2: &Time{
+					// 2022-09-25 01:00:00
+					time.Unix(1664038800, 0),
+				},
+			},
+			want: 1,
+		},
+		{
+			name: "",
+			args: args{
+				t1: &Time{
+					// 2022-09-26 01:00:00
+					time.Unix(1664125200, 0),
+				},
+				t2: &Time{
+					// 2022-09-24 01:00:00
+					time.Unix(1663952400, 0),
+				},
+			},
+			want: 2,
+		},
+		{
+			name: "",
+			args: args{
+				t1: &Time{
+					// 2022-09-26 01:00:00
+					time.Unix(1664125200, 0),
+				},
+				t2: &Time{
+					// 2022-09-25 23:00:00
+					time.Unix(1664118000, 0),
+				},
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := DiffDay(tt.args.t1, tt.args.t2); got != tt.want {
+				t.Errorf("DiffDay() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
